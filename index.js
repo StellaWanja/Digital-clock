a8f6fe3f9052c39d992d9b85401907014c2f1ead
StellaWanja <stylades@gmail.com>
2020-09-10 17:00:05 +0300

    alarm clock

--- index.js
+++ index.js
@@ -32,4 +32,53 @@ const clockTime = () => {
     setTimeout(clockTime, 1000);
 }
 
+const setAlarm = () => {
+    let alarmClock = document.getElementById("alarm-clock");
+    let alarmHour = document.getElementById("alarm-hour");
+    let alarmMin = document.getElementById("alarm-min");
+    let alarmSec = document.getElementById("alarm-sec");
+    let alarmSession = document.getElementById("alarm-session");
+    let alarmBtn = document.getElementById("alarm-btn");
+
+    let alarmElement;
+    let activeAlarm = false;
+    let alarmAudio = new Audio("Journey.mp3");
+    alarmAudio.loop = true;
+
+    const alarmMinSec = (id) => {
+        let select = id;
+        let maxMinSec = 59;
+
+        for (let i=0; i<=maxMinSec; i++){
+            select.options[select.options.length] = new Option(i<10 ? "0" + i: i);
+        }
+    }
+    const alarmHr = (id) => {
+        let select = id;
+        let maxHr = 12;
+
+        for (let i=0; i<=maxHr; i++){
+            select.options[select.options.length] = new Option(i<10 ? "0" + i: i);
+        }
+    }
+
+    alarmBtn.onclick = () => {
+        if (activeAlarm === false){
+            alarmHour.disabled = true;
+            alarmMin.disabled = true;
+            alarmSec.disabled = true;
+            alarmSession.disabled = true;
+
+            alarmElement = alarmHour.value + ":" + alarmMin.value + ":" + alarmSec.value + "" + alarmSession.value;
+            this.textContent = "Clear Alarm";
+            activeAlarm = true;
+        }
+    }
+
+    alarmMinSec(alarmSec);
+    alarmMinSec(alarmMin);
+    alarmHr(alarmHour);
+}
+
+setAlarm();
 clockTime();
